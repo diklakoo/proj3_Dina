e2804e0957dd953512885605814e87b9f9906d26
diklakoo <diklakoo@gmail.com>
2021-01-25 19:35:16 +0200

    exercise1 with function - final

--- main_births.py
+++ main_births.py
@@ -1,23 +1,41 @@
-d ={}
-with open('births.csv') as file:
-    for line in file:
-        if line.startswith('"year"'):
-            continue
-        else:
-            line=line.rstrip().split(",")
-            year=int(line[0])
-            state = line[1]
-            births = int(line[3])
-            if year >= 1986 and year <= 2006:
-                if state in d :
-                    d[state]= d[state]+ births
-                else:
-                    d[state] = births
+def parse_birth_file_to_dict(file_name):
+    births_per_state = {}
+    with open(file_name) as file:
+        for line in file:
+            if line.startswith('"year"'):
+                continue
+            else:
+                line = line.rstrip().split(",")
+                year = int(line[0])
+                state = line[1]
+                births = int(line[3])
+                if 1986 <= year <= 2006:
+                    if state in births_per_state:
+                        births_per_state[state] = births_per_state[state] + births
+                    else:
+                        births_per_state[state] = births
+    return births_per_state
 
-for key,val in d.items() :
-    print (f" {key} has {val:,} births total." )
+
+def print_birth_per_state(dict):
+    for key, val in dict.items():
+        print(f" between the years 1986-2006 , {key} has {val:,}  births total.")
+    print()
+
+def main():
+    birth_dict = parse_birth_file_to_dict('births.csv')
+    print_birth_per_state(birth_dict)
+    print_state_with_max_births(birth_dict)
 
 
-max_state = max(d, key=d.get)
-max_births =  max(d.values())
-print(f"the state with the most births is :  {max_state} , with {max_births: ,} births total .")
+
+def print_state_with_max_births(birth_dict):
+    max_state = max(birth_dict, key=birth_dict.get)
+    max_births = max(birth_dict.values())
+    print(f"the state with the max births is {max_state} , with {max_births: ,} births total .")
+
+
+if __name__ == '__main__':
+    main()
+
+
